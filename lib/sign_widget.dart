4deb5bc41ff0263cf52eb73cac0526a256a36b74
jiangtao.liang <446741151@qq.com>
2022-04-29 22:59:53 +0800

    [fix]导出到相册

--- lib/sign_widget.dart
+++ lib/sign_widget.dart
@@ -8,34 +8,41 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
+// import 'package:gallery_saver/gallery_saver.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:oktoast/oktoast.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'common_center_dialog.dart';
 
 /// 一张画布，可以跟随手指画线  例如用来做电子签名
-final  double STROKEWIDTH = 8;
+final double STROKEWIDTH = 8;
+
 class Signature extends StatefulWidget {
   Function? noSign;
   double? strokeWith;
 
-  Signature({this.noSign,this.strokeWith});
+  Signature({this.noSign, this.strokeWith});
 
   @override
   State<StatefulWidget> createState() {
-    return SignatureState(noSign! ,strokeWith ?? 1);
+    return SignatureState(noSign!, strokeWith ?? 1);
   }
 }
 
 class SignatureState extends State<Signature> {
   List<Offset?> _points = <Offset>[];
   GlobalKey _globalKey = GlobalKey();
+  GlobalKey _exportKey = GlobalKey();
 
   String _imageLocalPath = "";
   Function? noSign;
   double? mStrokeWidth;
 
-  SignatureState(this.noSign,this.mStrokeWidth);
+  SignatureState(this.noSign, this.mStrokeWidth);
+
+  String _save_2_album_success_tip = "";
 
   @override
   void initState() {
@@ -49,29 +56,40 @@ class SignatureState extends State<Signature> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        body: Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Stack(
+              children: [
+                Container(
+                  child:Text(_save_2_album_success_tip),
+                  padding: EdgeInsets.only(top: 70),
+                ),
 
-      body: Center(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: Stack(
-                children: [
-                  _Image(),
-                  _SignImage(),
-                  signWidget(),
-                ],
-              ),
+                RepaintBoundary(
+                    key: _exportKey,
+                    child: Container(child: Stack(children: <Widget>[
+                      _selectedImage(),
+                      _waterPrint(),
+                    ]))),
+                // _selectedImage(),
+                signWidget(),
+                _waterPrint(),
+              ],
             ),
-            Padding(
-              child: bottom(noSign!),
-              padding: EdgeInsets.only(right: 32),
-            ),
-          ],
-        ),
-      ));
+          ),
+          Padding(
+            child: _menuWidget(noSign!),
+            padding: EdgeInsets.only(right: 32),
+          ),
+        ],
+      ),
+    ));
   }
 
-  Widget bottom(Function noSign) {
+  // 菜单
+  Widget _menuWidget(Function noSign) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
@@ -81,6 +99,7 @@ class SignatureState extends State<Signature> {
             setState(() {
               _points.clear();
               _points = [];
+              _save_2_album_success_tip = "";
               // _imageLocalPath = "";
             });
           },
@@ -92,14 +111,14 @@ class SignatureState extends State<Signature> {
         RaisedButton(
           color: Theme.of(context).primaryColor,
           onPressed: () async {
-            if(_points != null && _points.length > 0){
+            if (_points != null && _points.length > 0) {
               File file = await _saveImageToFile();
-              String toPath = await _capturePng(context ,file);
+              String toPath = await _capturePng( _globalKey ,context, file);
               print("path:${file.path}");
               setState(() {
                 _imageLocalPath = toPath;
               });
-            }else{
+            } else {
               noSign();
             }
           },
@@ -108,7 +127,6 @@ class SignatureState extends State<Signature> {
             style: TextStyle(color: Colors.white),
           ),
         ),
-
         RaisedButton(
           color: Theme.of(context).primaryColor,
           onPressed: () async {
@@ -123,16 +141,26 @@ class SignatureState extends State<Signature> {
                   confirmFunction: (int callback) {
                     _getCameraImage();
                   },
-                  cancelFunction: (){
+                  cancelFunction: () {
                     _getImage();
                   },
                 );
               },
             );
-
           },
           child: Text(
             '选一张图',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        RaisedButton(
+          color: Theme.of(context).primaryColor,
+          onPressed: () async {
+            // 截取指定区域图片 保存到相册
+            _export2Album(context);
+          },
+          child: Text(
+            '导出到相册',
             style: TextStyle(color: Colors.white),
           ),
         ),
@@ -150,14 +178,17 @@ class SignatureState extends State<Signature> {
               onPanUpdate: (DragUpdateDetails details) {
                 RenderBox? renderBox = context.findRenderObject() as RenderBox?;
                 Offset? localPosition =
-                renderBox?.globalToLocal(details.globalPosition);
+                    renderBox?.globalToLocal(details.globalPosition);
                 RenderBox? referenceBox =
-                _globalKey.currentContext!.findRenderObject() as RenderBox?;
+                    _globalKey.currentContext!.findRenderObject() as RenderBox?;
 
                 //校验范围，防止超过外面
-                if (((localPosition?.dx) ?? 0) <= 0 || ((localPosition?.dy) ?? 0) <= 0) return;
-                if (((localPosition?.dx) ?? 0) > ((referenceBox?.size.width) ?? 0) ||
-                    ((localPosition?.dy) ?? 0) > ((referenceBox?.size.height) ?? 0)) return;
+                if (((localPosition?.dx) ?? 0) <= 0 ||
+                    ((localPosition?.dy) ?? 0) <= 0) return;
+                if (((localPosition?.dx) ?? 0) >
+                        ((referenceBox?.size.width) ?? 0) ||
+                    ((localPosition?.dy) ?? 0) >
+                        ((referenceBox?.size.height) ?? 0)) return;
 
                 setState(() {
                   _points = List.from(_points)..add(localPosition!);
@@ -168,21 +199,29 @@ class SignatureState extends State<Signature> {
               },
             ),
             Container(
-              child:  CustomPaint(
+              child: CustomPaint(
                 painter: SignatuePainter(_points, mStrokeWidth),
               ),
               color: Colors.red,
-            )
-            ,
-            // Image.file(
-            //   File(_imageLocalPath ?? ""),
-            //   height: 100,
-            //   width: 100,
-            // )
+            ),
           ],
         ),
       ),
     );
+  }
+
+  // 导出图片（带水印 到相册）
+  Future _export2Album(BuildContext context) async {
+     File file = await _saveImageToAlbumFile();
+     String toPath = await _capturePng( _exportKey, context, file);
+
+     await saveAlbum(toPath , context);
+     // print("path:${file.path}");
+     setState(() {
+       // _imageLocalPath = toPath;
+       _imageLocalPath = "";
+
+     });
   }
 
   Future<File> _saveImageToFile() async {
@@ -198,10 +237,23 @@ class SignatureState extends State<Signature> {
     return file;
   }
 
-  Future<String> _capturePng(BuildContext context , File file) async {
+  Future<File> _saveImageToAlbumFile() async {
+    Directory tempDir = await getTemporaryDirectory();
+    // Directory tempDir = await getExternalStorageDirectory();
+    int curT = DateTime.now().millisecondsSinceEpoch;
+    String toFilePath = "${tempDir.path}/$curT.png";
+    File file = File(toFilePath);
+    bool exists = await file.exists();
+    if (!exists) {
+      await file.create(recursive: true);
+    }
+    return file;
+  }
+
+  Future<String> _capturePng(GlobalKey key , BuildContext context, File file) async {
     //1.获取RenderRepaintBoundary
     RenderRepaintBoundary? boundary =
-    _globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary?;
+        key.currentContext!.findRenderObject() as RenderRepaintBoundary?;
     //2.生成Image  pixelRatio 不加上去，会很糊
     double pixratio = MediaQuery.of(context).devicePixelRatio;
     // ui.Image image = await boundary!.toImage(pixelRatio: (ui.window).devicePixelRatio);
@@ -219,9 +271,9 @@ class SignatureState extends State<Signature> {
   //用户本地图片
   File? _userImage;
 
-  Widget _Image() {
-    if(_userImage != null){
-      return  Container(
+  Widget _selectedImage() {
+    if (_userImage != null) {
+      return Container(
         padding: EdgeInsets.only(right: 10.0),
         alignment: Alignment.centerRight,
         // height: 300,
@@ -235,14 +287,21 @@ class SignatureState extends State<Signature> {
     );
   }
 
-  Widget _SignImage() {
-    if(_imageLocalPath != null && _imageLocalPath != ""){
+  // 右下角水印
+  Widget _waterPrint() {
+    if (_imageLocalPath != null && _imageLocalPath != "") {
       return Row(
         children: [
-          Expanded(flex: 1, child: Container(),),
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
           Column(
             children: [
-              Expanded(flex: 1, child: Container(),),
+              Expanded(
+                flex: 1,
+                child: Container(),
+              ),
               Container(
                 padding: EdgeInsets.only(right: 10.0),
                 // color: Colors.red,
@@ -281,30 +340,48 @@ class SignatureState extends State<Signature> {
   //选择相册
   Future _getImage() async {
     final pickerImages = await picker.getImage(source: ImageSource.gallery);
-    if(mounted){
+    if (mounted) {
       setState(() {
-        if(pickerImages != null){
+        if (pickerImages != null) {
           _userImage = File(pickerImages.path);
           print('你选择的本地路径是：${_userImage.toString()}');
-        }else{
+        } else {
           print('没有照片可以选择');
         }
       });
     }
   }
 
+  String albumName = 'Media';
 
+  saveAlbum(String? savePath , BuildContext context) async {
+    if(savePath == null || savePath == "") return;
+    await ImageGallerySaver.saveFile(savePath,
+        isReturnPathOfIOS: Platform.isAndroid ? false : true)
+        .then((result) {
+        _save_2_album_success_tip = "已保存到相册!!!";
+    });
+    // GallerySaver.saveImage(savePath, albumName: albumName)
+    //     .then((bool? success) {
+    //   setState(() {
+    //
+    //     // firstButtonText = 'image saved!';
+    //   });
+    // });
+
+
+  }
 }
 
 class SignatuePainter extends CustomPainter {
   List<Offset?> points = <Offset?>[];
-  double mStrokeWidth = 0 ;
+  double mStrokeWidth = 0;
 
-  SignatuePainter(this.points,mStrokeWidth);
+  SignatuePainter(this.points, mStrokeWidth);
 
   @override
   void paint(Canvas canvas, Size size) {
-    if(mStrokeWidth == null || mStrokeWidth == 0){
+    if (mStrokeWidth == null || mStrokeWidth == 0) {
       mStrokeWidth = STROKEWIDTH;
     }
     Paint paint = Paint()
